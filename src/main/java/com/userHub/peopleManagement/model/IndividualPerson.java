1580f320e8b55a10f40e465f5be69e932e844700
José Caio <j.de.araujo.cordeiro@accenture.com>
2025-06-06 16:43:23 -0300

    feat: starting the model structure of individuals and legal entities

--- src/main/java/com/userHub/peopleManagement/model/IndividualPerson.java
+++ src/main/java/com/userHub/peopleManagement/model/IndividualPerson.java
@@ -1,9 +1,42 @@
 package com.userHub.peopleManagement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
-public class IndividualPerson extends Person{
+@Entity
+@Table(name = "individual_person")
+public class IndividualPerson extends Person {
+    @NotBlank
     private String cpf;
+    @NotBlank
     private LocalDate birthDate;
+    @NotBlank
     private String rg;
+
+    public @NotBlank String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(@NotBlank String cpf) {
+        this.cpf = cpf;
+    }
+
+    public @NotBlank LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(@NotBlank LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
 }
